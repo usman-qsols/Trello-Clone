a52c8947e35044c01a691a4377a17acf4bf14a2d
usman-qsols <usmanrahim.qsols@gmail.com>
2023-12-15 08:40:29 +0500

    Workspace settings page added with css

--- app/(platform)/(dashboard)/_components/nav-item.tsx
+++ app/(platform)/(dashboard)/_components/nav-item.tsx
@@ -11,6 +11,7 @@ import {
 
 import { Activity, CreditCard, Layout, Settings } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export type Organization = {
   id: string;
@@ -104,5 +105,16 @@ export const NavItem = ({
         })}
       </AccordionContent>
     </AccordionItem>
+  );
+};
+
+NavItem.Skeleton = function SkeletonNavItem() {
+  return (
+    <div className="flex items-center gap-x-2">
+      <div className="w-10 h-10 relative shrink">
+        <Skeleton className="w-full h-full absolute" />
+      </div>
+      <Skeleton className="h-10 w-full" />
+    </div>
   );
 };
