7b367e7eedb6189655232ec9650d14f025d12893
usman-qsols <usmanrahim.qsols@gmail.com>
2023-12-19 01:05:30 +0500

    Working on server actions

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,5 +1,23 @@
+import { db } from "@/lib/db";
+import { create } from "@/actions/create-board";
+import { Button } from "@/components/ui/button";
+
 const OrganizationalIdPage = () => {
-  return <div>Organization</div>;
+  return (
+    <div>
+      <form action={create}>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          placeholder="Enter a board title"
+          className="border-black border p-1"
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </div>
+  );
 };
 
 export default OrganizationalIdPage;
