dfdf8d282b1655f334167f0de1c069b7123cd7b0
usman-qsols <usmanrahim.qsols@gmail.com>
2023-12-20 16:56:19 +0500

    Board Popover Form

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,8 +1,15 @@
 import { ClerkProvider } from "@clerk/nextjs";
+import { Tornado } from "lucide-react";
 import React from "react";
+import { Toaster } from "sonner";
 
 const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  return (
+    <ClerkProvider>
+      <Toaster />
+      {children}
+    </ClerkProvider>
+  );
 };
 
 export default PlatformLayout;
