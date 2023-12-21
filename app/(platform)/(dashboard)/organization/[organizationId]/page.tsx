d587f31e4ff074285ab894304b4c8f687d861d8d
usman-qsols <usmanrahim.qsols@gmail.com>
2023-12-21 15:27:24 +0500

    Board List

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,11 +1,18 @@
+import { Suspense } from "react";
 import { Info } from "./_components/Info";
 import { BoardList } from "./_components/board-list";
+import { Separator } from "@/components/ui/separator";
 
 const OrganizationalIdPage = async () => {
   return (
     <div className="w-full mb-20">
       <Info />
-      <BoardList />
+      <Separator className="my-4" />
+      <div className="px-2 md:px-4">
+        <Suspense fallback={<BoardList.Skeleton />}>
+          <BoardList />
+        </Suspense>
+      </div>
     </div>
   );
 };
