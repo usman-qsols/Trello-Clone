e9aff37e0bb3da23a3903c6e1192d96e5fff8669
usman-qsols <usmanrahim.qsols@gmail.com>
2024-01-27 17:08:01 +0500

    Card Modal added

--- app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
+++ app/(platform)/(dashboard)/board/[boardId]/_components/card-item.tsx
@@ -3,6 +3,7 @@
 import { Card } from "@prisma/client";
 import { Draggable } from "@hello-pangea/dnd";
 import { Provider } from "@radix-ui/react-tooltip";
+import { useCardModal } from "@/hooks/use-card-modal";
 
 interface CardItemProps {
   data: Card;
@@ -10,6 +11,7 @@ interface CardItemProps {
 }
 
 export const CardItem = ({ data, index }: CardItemProps) => {
+  const cardModal = useCardModal();
   return (
     <Draggable draggableId={data.id} index={index}>
       {(provided) => (
@@ -18,6 +20,7 @@ export const CardItem = ({ data, index }: CardItemProps) => {
           {...provided.dragHandleProps}
           ref={provided.innerRef}
           role="button"
+          onClick={() => cardModal.onOpen(data.id)}
           className="truncate border-2 border-transparent hover:border-black py-2 px-3 text-sm bg-white rounded-md shadow-sm"
         >
           {data.title}
